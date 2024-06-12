04126de44454bdb373dd2dc93571657c296dcfa0
karios2 <melvin.huaricacha@unmsm.edu.pe>
2024-06-12 17:40:38 -0500

    Primer Commit

---
+++ controller/usuarios.js
@@ -0,0 +1,52 @@
+const {response, request}= require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const {q,nombre = 'no envia',apikey} = req.query;
+    res.json({
+        msg: 'get API - controller',
+        q,
+        nombre,
+        apikey
+    });
+}
+
+
+
+const usuariosPut =(req, res= response) => {
+    const {id } = req.params; // params puede traer muchos datos.
+    res.json({
+        msg: 'put API - controller',
+        id
+    });
+}
+
+
+const usuariosPost = (req, res = response) => {
+    const {nombre,edad} = req.body;
+    res.json({
+        msg: 'post API - controller',
+        nombre,
+        edad
+    });
+}
+
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controller'
+    });
+}
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controller'
+    });
+}
+
+//se exporta un objeto pues van haber muchos
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
---
+++ models/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        //Middelware
+        this.middlewares();
+        //Rutas de la aplicacion
+        this.routes();
+        
+
+    }
+
+    
+
+    
+    //aque se define el metodo middleware que publicara la carpeta public
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+        //lectura y parseo del body recibe lo que se envia
+        this.app.use(express.json());
+        //directorio publico
+        this.app.use(express.static('public'));
+
+    }
+
+
+
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios'))
+    }
+
+
+    listen() {
+
+        this.app.listen(this.port, () =>{
+            console.log('Servidor corriendo en puerto ',this.port)
+        })
+    }
+
+}
+
+module.exports = Server;
